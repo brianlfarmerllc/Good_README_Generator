2c836d3fc4f56f73d3dea10d323be0366a8209d1
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-11 14:40:28 -0500

    npm install, set const for inquire,fs,and generateMarkdown

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
+const questions = [
+    "What is your Github user name?",
+    "What is the project Repo URL?",
+    "What is your project title?", 
+    "What is your project description?", 
+    "What are the project installation instructions?", 
+    "What is your projects intended usage?",
+    "What type of license would you like to assign?",
+    "How can others contribute?"
 ];
 
 function writeToFile(fileName, data) {
