236a53f1dcfbc083a1413a5476ab5585ee83b4a1
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-18 17:05:18 -0500

    removed contributor question, info pulled from repo name

---
+++ NEWREADME.md
@@ -0,0 +1,58 @@
+
+  # asdf
+
+  [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](brianlfarmerllc/Good_README_Generator)
+  
+  ## Description
+
+  asdf
+
+  ## Table of Contents
+  
+  * [Installation](#Installation)
+  * [Test](#Test)
+  * [Usage](#Usage)
+  * [Technologies](#Technologies)
+  * [License](#License)
+  * [Contributions](#Contributions)
+  
+  ## Installation
+
+  asdf
+
+  ## Test
+
+  asdf
+
+  ## Usage
+
+  The following image demonstrates the application functionality:
+
+  ![Add Description !!!!!!](asdf)}
+
+  ## Technologies
+
+  The following technologies were used in the developement of the application
+
+  
+
+  ## License
+
+  This application is licensed under the MIT license
+
+  ## Contributors
+
+  Contributor information and commit history can be seen here!
+  <a href="https://github.com/brianlfarmerllc/Good_README_Generator/graphs/contributors">
+    <img src="https://contributors-img.web.app/image?repo=brianlfarmerllc/Good_README_Generator" />
+  </a>
+
+  Made with [contributors-img](https://contributors-img.web.app).
+
+
+  undefined
+
+  ## Questions
+
+  If you have any questions about the application you can contact asdf directly @ asdf
+  
--- index.js
+++ index.js
@@ -3,119 +3,110 @@ const fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
-    "What is your Github user name?",
-    "What is your email",
-    "What is the project Repo URL?",
-    "What is your project title?", 
-    "What is your project description?", 
-    "What are the project installation instructions?", 
-    "How do you test the application",
-    "What is the path to your video showing your projects intended usage?",
-    "What types of technologies were used",
-    "What type of license would you like to assign?",
-    "Who contributed to this project?"
+  "What is your Github user name?",
+  "What is your email",
+  "What is the project Repo URL?",
+  "What is your project title?",
+  "What is your project description?",
+  "What are the project installation instructions?",
+  "How do you test the application",
+  "What is the path to your video showing your projects intended usage?",
+  "What types of technologies were used",
+  "What type of license would you like to assign?"
 ];
 
 
 function init() {
-    
-    inquirer.prompt([
-        {
-          type: "input",
-          name: "name",
-          message: questions[0]
-        },
-        {
-          type: "input",
-          name: "email",
-          message: questions[1]
-        },
-        {
-          type: "input",
-          name: "repo",
-          message: questions[2]
-        },
-        {
-          type: "input",
-          name: "title",
-          message: questions[3]
-        },
-        {
-          type: "input",
-          name: "description",
-          message: questions[4]
-        },
-        {
-          type: "input",
-          name: "install",
-          message: questions[5]
-        },
-        {
-          type: "input",
-          name: "test",
-          message: questions[6]
-        },
-        {
-          type: "input",
-          name: "usage",
-          message: questions[7]
-        },
-        {
-          type: "checkbox",
-          name: "tech",
-          message: questions[8],
-          choices:[
-              "HTML5",
-              "CSS",
-              "Javascript and jQuery",
-              "Moment",
-              "Bootstrap",
-              "Node.js",
-              "Inquirer",
-              "Express",
-              "Heroku",
-              "MYSQL",
-              "Handlebars",
-              "Sequeize",
-              "Mongo and Mongoose",
-              "Chart.js"   
-          ]
-        },
-        {
-          type: "list",
-          name: "license",
-          message: questions[9],
-          choices:[
-              "MIT",
-              "GPLv2",
-              "GPLv3",
-              "Apache",
-              "BSD 2-clause",
-              "BSD 3-clause",
-              "LGPLv3",
-              "AGPLv3",
-              "Unlicensed"
-          ]
-        },
-        {
-          type: "input",
-          name: "contribute",
-          message: questions[10]
-        },
-      ]).then(function (data) {
-        var fileName = "NEWREADME.md"
 
-        fs.writeFile(fileName, generateMarkdown(data), function(err) {
-      
-          if (err) {
-            return console.log(err);
-          }
-      
-        });
-      })
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: questions[0]
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions[1]
+    },
+    {
+      type: "input",
+      name: "repo",
+      message: questions[2]
+    },
+    {
+      type: "input",
+      name: "title",
+      message: questions[3]
+    },
+    {
+      type: "input",
+      name: "description",
+      message: questions[4]
+    },
+    {
+      type: "input",
+      name: "install",
+      message: questions[5]
+    },
+    {
+      type: "input",
+      name: "test",
+      message: questions[6]
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions[7]
+    },
+    {
+      type: "checkbox",
+      name: "tech",
+      message: questions[8],
+      choices: [
+        "HTML5",
+        "CSS",
+        "Javascript and jQuery",
+        "Moment",
+        "Bootstrap",
+        "Node.js",
+        "Inquirer",
+        "Express",
+        "Heroku",
+        "MYSQL",
+        "Handlebars",
+        "Sequeize",
+        "Mongo and Mongoose",
+        "Chart.js"
+      ]
+    },
+    {
+      type: "list",
+      name: "license",
+      message: questions[9],
+      choices: [
+        "MIT",
+        "GPLv2",
+        "GPLv3",
+        "Apache",
+        "BSD 2-clause",
+        "BSD 3-clause",
+        "LGPLv3",
+        "AGPLv3",
+        "Unlicensed"
+      ]
+    },
+  ]).then(function (data) {
+    var fileName = "NEWREADME.md"
 
-      
-    
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+
+      if (err) {
+        return console.log(err);
+      }
+
+    });
+  })
 }
 
 init();
